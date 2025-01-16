b4872cc5c52b890be6292f949bc8288eb2dca4d4
Cyrill Augustiny <c.augustiny@gmail.com>
2025-01-16 10:54:30 +0100

    Initialisierung Projekt und Tests
    Node Projekt wurde initialisiert und eine Simple Testvorlage erstelt

---
+++ tests/index.test.js
@@ -0,0 +1,8 @@
+//Test Suite
+describe("test suite", () => {
+  // Singular test inside Suite
+  test("example test", () => {
+    const testResult = 1 + 2;
+    expect(testResult).toBe(3);
+  });
+});
